16570a2295b73c1872e43783bbf210001736297c
Lukitis <149398424+Lukitis@users.noreply.github.com>
2024-01-03 10:24:15 -0400

    Add files via upload

---
+++ Fiibonacci.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int aux,antes,despues,actual,cont,suma,n;
+int main()
+{ 
+	printf("Ingrese el limite de numeros\n");
+	scanf("%d", &n);
+	antes=0;
+	despues=1;
+	cont=0;
+	
+	printf("%d\n%d\n",antes,despues);
+	while(cont<n)
+	{
+		actual=antes+despues;
+		printf("%d\n",actual);
+		//suma=actual+despues;
+		antes=despues;
+		despues=actual;
+		suma=actual+suma;
+		cont=cont+1;
+	}
+		aux=suma+1;
+		printf("La suma es: %d",aux);
+	return 0;
+}
+
